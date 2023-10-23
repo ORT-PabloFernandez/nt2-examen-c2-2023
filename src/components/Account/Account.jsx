1488a280c2883586cd2f308447886db9fcab06e1
martinrosbier <martinrosbier@gmail.com>
2023-10-23 20:02:07 -0300

    feat:  terminar componente account, TODO: el endpoint trae por _id no por numero de cuenta

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -1,15 +1,28 @@
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+
 const Customer = (props) => {
+    const { id } = useParams();
+    const [account, setAccount] = useState([]);
+
+    useEffect(()=>{
+       fetch(`https://analyticsbackendort.azurewebsites.net/api/accounts/${id}`)
+            .then(response => response.json())
+            .then(data => 
+                {
+                    setAccount(data);
+            })
+            .catch(error => console.log(error));
+    }, []);
+
+
 	return (
 		<div>
-			<ul key={props._id}>
-				<li>_id: {props._id}</li>
-				<li>username: {props.username}</li>
-				<li>name: {props.name}</li>
-				<li>address: {props.address}</li>
-				<li>birthdate: {props.birthdate}</li>
-				<li>email: {props.email}</li>
-				<li>accounts: {JSON.stringify(props.accounts)}</li>
+			<ul key={account._id}>
+				<li>limit: {account.limit}</li>
+				<li>products: {JSON.stringify(account.products)}</li>
 			</ul>
+            <Link to={`/customers`}>Go Back</Link>
 		</div>
 	);
 };
