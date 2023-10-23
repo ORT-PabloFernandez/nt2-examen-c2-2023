4a071f42b774480e672770bf6f0fc13afe138d17
martinrosbier <martinrosbier@gmail.com>
2023-10-23 20:09:52 -0300

    feat: conseguir el _id matcheando por account number / id

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -2,19 +2,19 @@ import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 
 const Customer = (props) => {
-    const { id } = useParams();
-    const [account, setAccount] = useState([]);
+	const { id } = useParams();
+	const [account, setAccount] = useState([]);
 
-    useEffect(()=>{
-       fetch(`https://analyticsbackendort.azurewebsites.net/api/accounts/${id}`)
-            .then(response => response.json())
-            .then(data => 
-                {
-                    setAccount(data);
-            })
-            .catch(error => console.log(error));
-    }, []);
+	useEffect(() => {
+		fetch(`https://analyticsbackendort.azurewebsites.net/api/accounts`)
+			.then((response) => response.json())
+			.then((data) => {
+				setAccount(data.find((item) => item.account_id == id));
+			})
+			.catch((error) => console.log(error));
+	}, []);
 
+	console.log(account);
 
 	return (
 		<div>
@@ -22,7 +22,7 @@ const Customer = (props) => {
 				<li>limit: {account.limit}</li>
 				<li>products: {JSON.stringify(account.products)}</li>
 			</ul>
-            <Link to={`/customers`}>Go Back</Link>
+			<Link to={`/customers`}>Go Back</Link>
 		</div>
 	);
 };
