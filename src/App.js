ad130f65a72456c32e3add82aa26554999d797c4
martinrosbier <martinrosbier@gmail.com>
2023-10-23 21:37:03 -0300

    fix: remover customer details que no estaba usado, mejorar home para que se lea

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
 import Account from './components/Account/Account';
 import CustomerPage from "./components/Customer/CustomerPage";
-import CustomerDetails from "./components/Customer/CustomerDetails";
 import { Link, BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<div className="container"><h1>Home</h1> <Link to="/customers">Customers</Link></div>} />
+				<Route path="/" element={<div className="container"><div className='card'><div className='card-body'><h1>Home</h1> <Link to="/customers">Customers</Link></div></div></div>} />
 				<Route path="/customers" element={<CustomerPage />} />
-				<Route path="/customers/:id" element={<CustomerDetails />} />
 				<Route path="/accounts/:id" element={<Account />} />
 			</Routes>
 		</BrowserRouter>
