6d874541a51ec669510ffc1caf9269d39b059d25
Facundo nelson <facundo.nelson@fluxit.com.ar>
2023-10-23 21:16:19 -0300

    refactorizo customerDetails

--- src/components/Customer/CustomerDetails.jsx
+++ src/components/Customer/CustomerDetails.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import Customer from './Customer'; // Importa el componente Customer
 
 function CustomerDetail() {
   const { id } = useParams();
@@ -21,25 +22,9 @@ function CustomerDetail() {
 
   return (
     <div>
-      <h2>Detalles del Cliente</h2>
       {customerData ? (
-        <div>
-          <p><strong>Username:</strong> {customerData.username}</p>
-          <p><strong>Nombre:</strong> {customerData.name}</p>
-          <p><strong>Dirección:</strong> {customerData.address}</p>
-          <p><strong>Fecha de Nacimiento:</strong> {customerData.birthdate}</p>
-          <p><strong>Correo Electrónico:</strong> {customerData.email}</p>
-          <p><strong>Cuentas:</strong></p>
-          <ul>
-            {customerData.accounts.map((account, index) => (
-              <li key={index}>
-                <Link to={`/customers/${id}/accounts/${account}`}>
-                  {account}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+        // Renderiza el componente Customer con los datos del cliente
+        <Customer customerData={customerData} />
       ) : (
         <p>Cargando datos del cliente...</p>
       )}
